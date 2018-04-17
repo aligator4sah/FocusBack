cbbb28dbcb59456f173807689f85704160397fa7
sawyerbutton <lil103@pitt.edu>
2018-04-17 15:59:47 -0400

    modified the findall function

--- src/CommunityMembers/Interfaces/ICommunityMember.service.ts
+++ src/CommunityMembers/Interfaces/ICommunityMember.service.ts
@@ -2,7 +2,7 @@ import {ICommunityMember} from "./ICommunityMember";
 import { CommunityMemberEntity} from "../communityMember.entity";
 
 export interface ICommunityMemberService {
-    getAllCommunityMember():Promise<Array<CommunityMemberEntity>>;
+    getAllCommunityMember():Promise<[Array<CommunityMemberEntity>,number]>;
     getCommunityMember(id: number): Promise<CommunityMemberEntity | null>;
     addCommunityMember(communityMember: ICommunityMember): Promise<CommunityMemberEntity>;
     updateCommunityMember(id: number, newCommunityMember: ICommunityMember): Promise<CommunityMemberEntity | null>;
