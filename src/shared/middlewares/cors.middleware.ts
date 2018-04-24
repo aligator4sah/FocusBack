ead8a9969084dbfdf2de828b4417798f8db431a9
sawyerbutton <lil103@pitt.edu>
2018-04-24 17:12:08 -0400

    modifeid the resheader

--- src/shared/middlewares/cors.middleware.ts
+++ src/shared/middlewares/cors.middleware.ts
@@ -6,10 +6,14 @@ export class CorsMiddleware implements NestMiddleware {
         return(req, res, next) => {
             let allowedOrigins = ["http://localhost:3000","http://localhost:4200"];
             if (allowedOrigins.indexOf(req.header("Origin")) > -1) {
-                res.header("Access-Control-Allow-Origin", req.header("Origin"));
-                res.header("Access-Control-Allow-Headers", "content-types,Access-Control-Allow-Headers");
-                res.header("Access-Control-Allow-Methods", "POST,GET,DELETE,PATCH,OPTIONS");
-                res.header("Content-Type","application/json;charset=utf-8");
+                res.setheader("Access-Control-Allow-Origin", req.header("Origin"));
+                res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+                // Request headers you wish to allow
+                res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+                // Set to true if you need the website to include cookies in the requests sent
+                // to the API (e.g. in case you use sessions)
+                res.setHeader('Access-Control-Allow-Credentials', true);
+                res.setheader("Content-Type","application/json;charset=utf-8");
             }
             next();
         };
